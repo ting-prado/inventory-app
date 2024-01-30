9c191b6750b9a4e015fb6ea55c068fd583aa1518
tingprado <ethel.prado1998@gmail.com>
2024-01-30 00:10:50 -0800

    Converted to promises to async/await

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,17 +1,37 @@
 const Product = require("../models/product");
 const Branch = require("../models/branch");
 const asyncHandler = require("express-async-handler");
+const ObjectId = require("mongodb").ObjectId;
 
 // Display all products
 exports.product_list = asyncHandler(async (req, res, next) => {
-	const productList = await Product.find();
+	const productList = await Product.aggregate([
+		{
+			$lookup: {
+				from: "branches",
+				localField: "distribution.branchId",
+				foreignField: "_id",
+				as: "branchDetails"
+			}
+		}
+	]);
 
-	const response = productList.map((product) => ({
-		...product._doc,
-		undistributed:
-			product.totalCount -
-			product.distribution.reduce((sum, i) => sum + i.count, 0)
-	}));
+	const response = productList.map((product) => {
+		const newData = {
+			...product,
+			distribution: product.distribution.map((branch, idx) => ({
+				...branch,
+				branchName: product.branchDetails[idx].branchName
+			})),
+			undistributed:
+				product.totalCount -
+				product.distribution.reduce((sum, i) => sum + i.count, 0)
+		};
+
+		delete newData.branchDetails;
+
+		return newData;
+	});
 
 	res.send(response);
 });
@@ -41,7 +61,8 @@ exports.add_product = asyncHandler(async (req, res, next) => {
 		totalCount: sum
 	});
 
-	product.save().then(() => res.send("Product created."));
+	await product.save();
+	res.send("Product created.");
 });
 
 // Edit product
@@ -63,12 +84,16 @@ exports.edit_product = asyncHandler(async (req, res, next) => {
 					.map((item) => ({ branchId: item._id, count: 0 }))
 			)
 		}
-	).then(() => res.send("Product updated"));
+	);
+
+	res.send("Product updated");
 });
 
 // Delete product
 exports.delete_product = asyncHandler(async (req, res, next) => {
 	const id = req.params.id;
 
-	Product.deleteOne({ _id: id }).then((result) => res.send("Product deleted."));
+	await Product.deleteOne({ _id: id });
+
+	res.send("Product deleted.");
 });
